43358504983133e5a4eb48eccc9213a592af3eb2
Ester Ribeiro dos Santos <ester.santos@teadit.com>
2021-03-21 22:58:45 -0300

    Singleton

---
+++ FundamentosDeSoftware/Singleton/Pattern/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonProject.Pattern
+{
+    //sealded - impede que outras classes herdem dela
+    public sealed class Singleton
+    {
+        private static Singleton _instancia = null;
+
+        public static Singleton GetInstancia
+        {
+            get
+            { 
+                if(_instancia == null)
+                {
+                    _instancia = new Singleton();
+                    Console.WriteLine("Bola em jogo");
+                }
+
+                return _instancia;
+            }
+        }
+
+        public void Mensagem(string mensg)
+        {
+            Console.WriteLine(mensg);
+        }
+    }
+}
--- FundamentosDeSoftware/Singleton/Program.cs
+++ FundamentosDeSoftware/Singleton/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using SingletonProject.Pattern;
+using System;
 
-namespace Singleton
+
+namespace SingletonProject
 {
+    /// <summary>
+    /// Singleton - escopo objeto
+    /// Singleton - Garantir que apenas uma única instância de objeto seja criada
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Singleton jogador1 = Singleton.GetInstancia;
+            jogador1.Mensagem("A bola está comigo no meio do campo!");
+
+            Singleton jogador2 = Singleton.GetInstancia;
+            jogador2.Mensagem("Jogador2 recebeu a bola");
+
+            Singleton jogador3 = Singleton.GetInstancia;
+            jogador3.Mensagem("Jogador3 tomou a bola");
+
+            Console.ReadKey();
         }
     }
 }
